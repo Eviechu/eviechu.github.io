ebebc2191637c2f2b0d5753302818c01eccdab49
Evellyn <eviechu@tutanota.com>
2022-12-07 04:35:49 +0000

    JS Fix

--- javascript/mobileDetection.js
+++ javascript/mobileDetection.js
@@ -1,8 +1,17 @@
-function toggleTheme(node) {
+function indexTheme() {
     if (/Android|iPhone/i.test(navigator.userAgent)) {
     // This checks if the current device is in fact mobile
-        
+        document.getElementById("theme").href="css/mobile.css";
     } else {
-        
+        document.getElementById("theme").href="css/main.css";
+    }
+};
+
+function switchTheme() {
+    if (/Android|iPhone/i.test(navigator.userAgent)) {
+    // This checks if the current device is in fact mobile
+        document.getElementById("theme").href="../css/mobile.css";
+    } else {
+        document.getElementById("theme").href="../css/main.css";
     }
 };

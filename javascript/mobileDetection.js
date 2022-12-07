4ef653b0892f77fe004cf3bb01f492758643a1d0
Evellyn <eviechu@tutanota.com>
2022-12-07 03:50:10 +0000

    Update

--- javascript/mobileDetection.js
+++ javascript/mobileDetection.js
@@ -1,8 +1,8 @@
 function toggleTheme(node) {
     if (/Android|iPhone/i.test(navigator.userAgent)) {
     // This checks if the current device is in fact mobile
-        node.rel = 'stylesheet-mobile';
+        
     } else {
-        node.rel = 'stylesheet';
+        
     }
 };
